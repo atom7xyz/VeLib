ff7fa0de93d11d2da8210088e22e5ad701bed8b6
anton <contact@sorridi.xyz>
2023-07-24 15:23:41 +0200

    Bump velocity-api to 3.2.0-SNAPSHOT

--- velib-velocity/build.gradle.kts
+++ velib-velocity/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
     /* Velocity related */
-    compileOnly("com.velocitypowered:velocity-api:3.1.1")
-    annotationProcessor("com.velocitypowered:velocity-api:3.1.1")
+    compileOnly("com.velocitypowered:velocity-api:3.2.0-SNAPSHOT")
+    annotationProcessor("com.velocitypowered:velocity-api:3.2.0-SNAPSHOT")
 }
